eb6b285a1623b180bcb4faa059434dc662681831
Santosh Pattnaik <santoshpattnaik91@gmail.com>
2022-10-23 18:54:58 +0530

    added sum,difference,multiply and divide operations

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include<windows.h>
 
 constexpr int DISPLAY = 1 ;
+constexpr int SUM = 2 ;
+constexpr int DIFF = 3 ;
+constexpr int MULT = 4 ;
+constexpr int QUOTIENT = 5 ;
 HWND textView, op1, op2;
 
 LRESULT CALLBACK WindowProcedure(HWND, UINT, WPARAM, LPARAM);
@@ -43,16 +47,48 @@ LRESULT CALLBACK WindowProcedure(HWND handle, UINT msg, WPARAM wParam, LPARAM lP
 	switch (msg) {
 	case WM_COMMAND:
 		switch (wParam) {
-		case DISPLAY:
+			int result;
 			WCHAR operand1[100], operand2[100];
+			WCHAR buffer[100];
+		case SUM:
 			GetWindowTextW(op1, operand1, 100);
 			GetWindowTextW(op2, operand2, 100);
-			int result = _wtoi(operand1) + _wtoi(operand2);
-			//wcscat_s(operand1, operand2);
-			WCHAR buffer[100];
+			
+			result = _wtoi(operand1) + _wtoi(operand2);
+
 			wsprintf(buffer, L"%d", result);
 
 			SetWindowTextW(textView,buffer);
+			break;
+		case DIFF:
+			GetWindowTextW(op1, operand1, 100);
+			GetWindowTextW(op2, operand2, 100);
+
+			result = _wtoi(operand1) - _wtoi(operand2);
+
+			wsprintf(buffer, L"%d", result);
+
+			SetWindowTextW(textView, buffer);
+			break;
+		case MULT:
+			GetWindowTextW(op1, operand1, 100);
+			GetWindowTextW(op2, operand2, 100);
+
+			result = _wtoi(operand1) * _wtoi(operand2);
+
+			wsprintf(buffer, L"%d", result);
+
+			SetWindowTextW(textView, buffer);
+			break;
+		case QUOTIENT:
+			GetWindowTextW(op1, operand1, 100);
+			GetWindowTextW(op2, operand2, 100);
+
+			result = _wtoi(operand1) / _wtoi(operand2);
+
+			wsprintf(buffer, L"%d", result);
+
+			SetWindowTextW(textView, buffer);
 			break;
 		}
 		break;
@@ -68,8 +104,14 @@ LRESULT CALLBACK WindowProcedure(HWND handle, UINT msg, WPARAM wParam, LPARAM lP
 }
 
 void addControls(HWND handle) {
-	textView=CreateWindowW(L"Edit", L"", WS_CHILD | WS_VISIBLE|WS_BORDER, 100, 50, 1000, 100, handle, NULL, NULL, NULL);
-	op1=CreateWindowW(L"Edit", L"", WS_CHILD | WS_VISIBLE|WS_BORDER|ES_NUMBER, 100, 200,90, 100, handle, NULL, NULL, NULL);
-	op2=CreateWindowW(L"Edit", L"", WS_CHILD | WS_VISIBLE|WS_BORDER|ES_NUMBER, 500, 200,90, 100, handle, NULL, NULL, NULL);
-	CreateWindowW(L"Button", L"Calculate", WS_VISIBLE | WS_CHILD, 650, 400, 70, 30, handle, (HMENU)DISPLAY, NULL, NULL);
+	textView=CreateWindowW(L"Edit", L"", WS_CHILD | WS_VISIBLE|WS_BORDER|ES_CENTER, 100, 50, 1000, 100, handle, NULL, NULL, NULL);
+	op1=CreateWindowW(L"Edit", L"", WS_CHILD | WS_VISIBLE|WS_BORDER|ES_CENTER, 100, 200,90, 100, handle, NULL, NULL, NULL);
+	op2=CreateWindowW(L"Edit", L"", WS_CHILD | WS_VISIBLE|WS_BORDER|ES_CENTER, 500, 200,90, 100, handle, NULL, NULL, NULL);
+	CreateWindowW(L"Button", L"Calculate", WS_VISIBLE | WS_CHILD, 650, 200, 70, 30, handle, (HMENU)DISPLAY, NULL, NULL);
+
+	//operator buttons
+	CreateWindowW(L"Button", L"+", WS_VISIBLE | WS_CHILD, 650, 240, 30, 30, handle, (HMENU)SUM, NULL, NULL);
+	CreateWindowW(L"Button", L"-", WS_VISIBLE | WS_CHILD, 650, 280, 30, 30, handle, (HMENU)DIFF, NULL, NULL);
+	CreateWindowW(L"Button", L"*", WS_VISIBLE | WS_CHILD, 650, 320, 30, 30, handle, (HMENU)MULT, NULL, NULL);
+	CreateWindowW(L"Button", L"/", WS_VISIBLE | WS_CHILD, 650, 360, 30, 30, handle, (HMENU)QUOTIENT, NULL, NULL);
 }
